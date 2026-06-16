3155a3c563b8e9597eb446a991d0df2a14c18dc8
IfanPrasetyoAji <ifan.prasetyo.aji@gmail.com>
2026-06-16 18:11:09 +0700

    refactor: make gravity.c use raymath for Vector3 calculations

--- src/physics/gravity.c
+++ src/physics/gravity.c
@@ -1,16 +1,13 @@
 #include "body.c"
 #include "raylib.h"
-#include <math.h>
+#include "raymath.h"
 
 Vector3 calculateGravitationalForce(Body body1, Body body2) {
   Vector3 force = {0.0f, 0.0f, 0.0f};
 
-  Vector3 direction = {body2.position.x - body1.position.x,
-                       body2.position.y - body1.position.y,
-                       body2.position.z - body1.position.z};
+  Vector3 direction = Vector3Subtract(body1.position, body2.position);
 
-  float distance = sqrt(direction.x * direction.x + direction.y * direction.y +
-                        direction.z * direction.z);
+  float distance = Vector3Distance(body1.position, body2.position);
 
   if (distance == 0.0f) {
     return force;
@@ -24,16 +21,14 @@ Vector3 calculateGravitationalForce(Body body1, Body body2) {
   Vector3 unitVector = {direction.x / distance, direction.y / distance,
                         direction.z / distance};
 
-  force.x = unitVector.x * forceMultiplier;
-  force.y = unitVector.y * forceMultiplier;
-  force.z = unitVector.z * forceMultiplier;
+  force = Vector3Scale(unitVector, forceMultiplier);
 
   return force;
 }
 
 void resetAcceleration(Body bodies[], int count) {
   for (int i = 0; i < count; i++) {
-    bodies[i].acceleration = (Vector3){0.0f, 0.0f, 0.0f};
+    bodies[i].acceleration = Vector3Zero();
   }
 }
 
@@ -43,13 +38,9 @@ void applyGravity(Body bodies[], int count) {
       Vector3 forceThatBe = calculateGravitationalForce(bodies[i], bodies[j]);
 
       // Apply the force that be to both bodies
-      bodies[i].acceleration.x += forceThatBe.x;
-      bodies[i].acceleration.y += forceThatBe.y;
-      bodies[i].acceleration.z += forceThatBe.z;
-
-      bodies[j].acceleration.x -= forceThatBe.x;
-      bodies[j].acceleration.y -= forceThatBe.y;
-      bodies[j].acceleration.z -= forceThatBe.z;
+      bodies[i].acceleration = Vector3Add(bodies[i].acceleration, forceThatBe);
+      bodies[j].acceleration =
+          Vector3Subtract(bodies[j].acceleration, forceThatBe);
     }
   }
 }
